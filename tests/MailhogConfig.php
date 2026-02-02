dedff156cb5f6df03280733e5488aa9b7c771ebc
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-02-02 12:38:38 -0300

    chore: rename environments
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- tests/MailhogConfig.php
+++ tests/MailhogConfig.php
@@ -37,21 +37,21 @@ final class MailhogConfig
             return;
         }
 
-        if (!isset($_ENV['mailhog_smtp_dsn'])) {
-            throw new RuntimeException('Environment variable "mailhog_smtp_dsn" must be defined');
+        if (!isset($_ENV['mailpit_smtp_dsn'])) {
+            throw new RuntimeException('Environment variable "mailpit_smtp_dsn" must be defined');
         }
 
-        $info = parse_url($_ENV['mailhog_smtp_dsn']);
+        $info = parse_url($_ENV['mailpit_smtp_dsn']);
 
         if (!is_array($info)) {
-            throw new RuntimeException(sprintf('Unable to parse DSN "%s"', $_ENV['mailhog_smtp_dsn']));
+            throw new RuntimeException(sprintf('Unable to parse DSN "%s"', $_ENV['mailpit_smtp_dsn']));
         }
 
         if (!isset($info['host'])) {
             throw new RuntimeException(
                 sprintf(
                     'Unable to parse host from Mailhog DSN "%s"',
-                    $_ENV['mailhog_smtp_dsn']
+                    $_ENV['mailpit_smtp_dsn']
                 )
             );
         }
@@ -60,7 +60,7 @@ final class MailhogConfig
             throw new RuntimeException(
                 sprintf(
                     'Unable to parse port from Mailhog DSN "%s"',
-                    $_ENV['mailhog_smtp_dsn']
+                    $_ENV['mailpit_smtp_dsn']
                 )
             );
         }
